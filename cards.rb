0c980d93beabb90d646ab4ea8a90f89c34d87f67
Chris Salvi <csalvi42@gmail.com>
2015-02-01 14:01:49 -0800

    pt 2

--- cards.rb
+++ cards.rb
@@ -1,5 +1,11 @@
 cards = ["Jack", "Queen", "King", "Ace", "Joker"]
 
-cards.each do |c|
+cards.reverse_each do |c|
   puts "#{c.upcase} - #{c.length}"
+end
+
+scores = {"Larry" => 10, "Moe" => 8, "Curly" => 12}
+
+scores.each do |k,v|
+  puts "#{k} scored a #{v}"
 end
