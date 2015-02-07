a2ea98b0fe181cf4090d410a584f8804798b7fc7
Chris Salvi <csalvi42@gmail.com>
2015-02-07 12:34:12 -0800

    heavily edited Flyer class

--- main.rb
+++ main.rb
@@ -1,44 +1,52 @@
 class Flyer
   attr_reader :name, :email, :miles_flown
+  attr_accessor :status
 
-  def initialize(name, email, miles_flown)
+  def initialize(name, email, miles_flown, status=:bronze)
     @name = name
     @email = email
     @miles_flown = miles_flown
+    @status = status
   end
 
   def to_s
-    "#{name} (#{email}): #{miles_flown}"
+    "#{name} (#{email}): #{miles_flown} - #{status}"
   end
 end
 
 flyers = []
+flyers << Flyer.new("Larry", "larry@example.com", 4000, :platinum)
+flyers << Flyer.new("Moe", "moe@example.com", 1000)
+flyers << Flyer.new("Curly", "curly@example.com", 3000, :gold)
+flyers << Flyer.new("Shemp", "shemp@example.com", 2000)
 
-1.upto(5) do |n|
-  flyers << Flyer.new("Flyer #{n}", "flyer#{n}@example.com", n * 1000)
-end
+# flyers = []
 
-flyers.each do |f|
-  puts "#{f.name} - #{f.miles_flown} miles"
-end
+# 1.upto(5) do |n|
+#   flyers << Flyer.new("Flyer #{n}", "flyer#{n}@example.com", n * 1000)
+# end
 
-total = 0
+# flyers.each do |f|
+#   puts "#{f.name} - #{f.miles_flown} miles"
+# end
 
-flyers.each do |f|
-  total+= f.miles_flown
-end
+# total = 0
 
-puts "Total miles flown: #{total}"
+# flyers.each do |f|
+#   total+= f.miles_flown
+# end
 
-promotions = { "United" => 1.5, "Delta" => 2.0, "Lufthansa" => 2.5 }
+# puts "Total miles flown: #{total}"
 
-promotions.each do |k,v|
-  puts "Earn #{v}x miles by flying #{k}!"
-end
+# promotions = { "United" => 1.5, "Delta" => 2.0, "Lufthansa" => 2.5 }
 
-flyers.each do |flyer|
-  promotions.each do |airline, multiplier|
-    puts "#{flyer.name} could earn #{flyer.miles_flown * multiplier} miles by flying #{airline}!"
-  end
-end
+# promotions.each do |k,v|
+#   puts "Earn #{v}x miles by flying #{k}!"
+# end
+
+# flyers.each do |flyer|
+#   promotions.each do |airline, multiplier|
+#     puts "#{flyer.name} could earn #{flyer.miles_flown * multiplier} miles by flying #{airline}!"
+#   end
+# end
 
