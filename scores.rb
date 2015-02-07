405bcf54b043a6ffd3559af416bb7f9b05212a4a
Chris Salvi <csalvi42@gmail.com>
2015-02-07 14:31:23 -0800

    started module with reduce and map enumerable

--- scores.rb
+++ scores.rb
@@ -1,29 +1,34 @@
-scores = [83,71,92,64,98,87,75,69]
+# scores = [83,71,92,64,98,87,75,69]
 
-high_scores = scores.select {|n| n>80}
+# high_scores = scores.select {|n| n>80}
 
-puts high_scores
+# puts high_scores
 
-puts "-----"
+# puts "-----"
 
-low_scores = scores.reject {|n| n>80}
+# low_scores = scores.reject {|n| n>80}
 
-puts low_scores
+# puts low_scores
 
-puts "-----"
+# puts "-----"
 
-puts scores.any? { |n| n < 70}
+# puts scores.any? { |n| n < 70}
 
-puts "-----"
+# puts "-----"
 
-puts scores.detect {|n| n < 70}
+# puts scores.detect {|n| n < 70}
 
-puts "-----"
+# puts "-----"
 
-scores.select! { |score| score > 70 }
-puts scores
+# scores.select! { |score| score > 70 }
+# puts scores
 
-puts "-----"
+# puts "-----"
 
-scores.reject! { |score| score.even? }
-p scores
+# scores.reject! { |score| score.even? }
+# p scores
+
+scores = [83, 71, 92, 64, 98, 87, 75, 69]
+
+scores_doubled = scores.map {|n| n * 2}
+puts scores_doubled
