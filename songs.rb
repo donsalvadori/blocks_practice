f0a2932fcd1e513166a952026b5970b1b91cded8
Chris Salvi <csalvi42@gmail.com>
2015-03-03 12:47:25 -0800

    practiced yielding to enumerables

--- songs.rb
+++ songs.rb
@@ -17,6 +17,8 @@ song2 = Song.new("Ramblin' Man", "Allman Bros", 7)
 song3 = Song.new("Layla", "Eric Clapton", 6)
 
 class Playlist
+  include Enumerable
+  
   def initialize(name)
     @name = name
     @songs = []
@@ -25,4 +27,27 @@ class Playlist
   def add_song(song)
     @songs << song
   end
+
+  def each
+    @songs.each { |song| yield song }
+  end
+
+  def play_songs
+    each { |song| song.play }
+  end
 end
+
+playlist = Playlist.new("Classic Hits")
+playlist.add_song(song1)
+playlist.add_song(song2)
+playlist.add_song(song3)
+
+playlist.each { |song| song.play }
+playlist.play_songs
+
+
+prince_songs = playlist.select { |song| song.artist =~ /Prince/ }
+p prince_songs
+
+total_duration = playlist.reduce(0) { |sum, song| sum + song.duration }
+p total_duration
