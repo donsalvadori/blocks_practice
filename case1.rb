8992d5defabbef481408d9266ea9433187cf88ce
Chris Salvi <csalvi42@gmail.com>
2015-03-07 10:47:57 -0800

    sensor class

--- case1.rb
+++ case1.rb
@@ -1,13 +1,35 @@
-def time_it
+def time_it(label)
   start_time = Time.now
   yield
   elapsed_time = Time.now - start_time
-  puts "It took #{elapsed_time} seconds"
+  puts "#{label} took #{elapsed_time} seconds"
 end
 
 
 
-time_it do
+time_it("Sleepy code") do
   #run some code
   sleep(0.5)
+end
+
+#_______________________________
+
+class Sensor
+  def temperature
+    rand(100..200)
+  end
+
+  def level
+    rand(1..5)
+  end
+end
+
+sensor = Sensor.new
+
+puts "Checking water temperature..."
+result = sensor.temperature < 150
+if result
+  puts "OK"
+else
+  puts "FAILED!"
 end
